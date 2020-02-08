726338c93d8f3677e4be85418c3f604f01b328d2
Altay Aydemir <altay.aydemir@gmail.com>
2020-02-08 08:03:22 +0100

    update config to include ts setup

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = {
-  extends: ['airbnb', 'prettier', 'prettier/react'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+
   plugins: ['prettier', 'react-hooks'],
+
   env: {
     browser: true,
     commonjs: true,
@@ -8,43 +15,118 @@ module.exports = {
     jest: true,
     node: true,
   },
+
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+
   rules: {
     'prettier/prettier': 'error',
 
-    'import/no-cycle': 1,
+    camelcase: 'off',
 
-    'no-use-before-define': 0,
-    'no-return-assign': 1,
-    'no-param-reassign': 0,
-    'no-shadow': 0,
-    'no-unused-vars': 1,
-    'consistent-return': 0,
-    'no-restricted-syntax': 1,
-    'max-classes-per-file': 1,
-    'operator-assignment': 1,
+    'no-use-before-define': 'off',
+    'no-return-assign': 'warn',
+    'no-param-reassign': 'off',
+    'no-shadow': 'off',
+    'no-unused-vars': 'off',
+    'no-restricted-syntax': 'warn',
 
-    'jsx-a11y/accessible-emoji': 1,
-    'jsx-a11y/no-static-element-interactions': 1,
-    'jsx-a11y/no-noninteractive-element-interactions': 1,
-    'jsx-a11y/media-has-caption': 1,
-    'jsx-a11y/mouse-events-have-key-events': 1,
-    'jsx-a11y/click-events-have-key-events': 1,
-    'jsx-a11y/anchor-is-valid': 1,
-    'jsx-a11y/label-has-for': 1,
+    'consistent-return': 'off',
+    'max-classes-per-file': 'warn',
+    'operator-assignment': 'warn',
+
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/interface-name-prefix': [
+      'error',
+      {
+        prefixWithI: 'always',
+      },
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+
+    'import/no-cycle': 'warn',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'tests/**',
+          '**/*.stories.jsx',
+          '**/*.stories.tsx',
+          '**/*.spec.js',
+          '**/*.spec.ts',
+        ],
+      },
+    ],
+
+    'jsx-a11y/no-autofocus': 'off',
+    'jsx-a11y/accessible-emoji': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
+    'jsx-a11y/no-noninteractive-element-interactions': 'warn',
+    'jsx-a11y/media-has-caption': 'warn',
+    'jsx-a11y/mouse-events-have-key-events': 'warn',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'jsx-a11y/label-has-for': 'warn',
+
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-curly-brace-presence': 'warn',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
 
     'react/prefer-stateless-function': [2, { ignorePureComponents: true }],
-    'react/forbid-prop-types': 0,
-    'react/default-props-match-prop-types': 1,
-    'react/no-unused-state': 1,
-    'react/jsx-curly-brace-presence': 1,
-    'react/destructuring-assignment': 0,
+    'react/forbid-prop-types': 'off',
+    'react/default-props-match-prop-types': 'warn',
+    'react/no-unused-state': 'warn',
 
-    'react/jsx-props-no-spreading': 1,
-    'react/state-in-constructor': 1,
-    'react/no-deprecated': 1,
-    'react/static-property-placement': 1,
+    'react/destructuring-assignment': 'off',
 
-    'react-hooks/rules-of-hooks': 1,
-    'react-hooks/exhaustive-deps': 1,
+    'react/state-in-constructor': 'warn',
+    'react/no-deprecated': 'warn',
+    'react/static-property-placement': 'warn',
+
+    'react-hooks/rules-of-hooks': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    'no-unused-vars': 'off',
   },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 }
