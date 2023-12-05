4dda1833691bff37b6339c06e43450c3cfa065ee
Altay <altay@zebrastik.com>
2023-12-05 12:42:57 +0300

    feat: leave formatting to prettier

--- formatting.js
+++ formatting.js
@@ -1,16 +1,5 @@
-require('@rushstack/eslint-config/patch/modern-module-resolution')
+require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['plugin:perfectionist/recommended-natural'],
-  plugins: ['prettier', 'json-files'],
-  overrides: [
-    {
-      files: ['*.jsx', '*.js', '*.ts', '*.tsx', '*.json'],
-    },
-  ],
-  rules: {
-    'json-files/eol-last': 'error',
-    'json-files/sort-package-json': 'error',
-    'prettier/prettier': 'error',
-  },
-}
+  extends: ['plugin:perfectionist/recommended-natural', 'prettier'],
+};
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('@rushstack/eslint-config/patch/modern-module-resolution')
-
-const formatting = require('./formatting')
+require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['react-app', 'react-app/jest', ...formatting.extends],
-  plugins: formatting.plugins,
-  overrides: formatting.overrides,
-  rules: formatting.rules,
-}
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'prettier',
+    'plugin:perfectionist/recommended-natural',
+  ],
+};
